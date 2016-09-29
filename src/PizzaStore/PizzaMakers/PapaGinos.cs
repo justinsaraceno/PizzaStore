93c3a19cb70e3450c7888b822fd977e71671acf1
Justin Saraceno <justin.saraceno@gmail.com>
2016-09-29 13:54:46 -0400

    step 4 - adding pizza makers

---
+++ src/PizzaStore/PizzaMakers/PapaGinos.cs
@@ -0,0 +1,17 @@
+﻿using PizzaDomain;
+using System;
+using System.Collections.Generic;
+
+namespace PizzaStore.PizzaMakers
+{
+    public class PapaGinos : IPizzaMaker
+    {
+        public void TakeOrder(Size size, List<string> toppings)
+        {
+            // this would process some pizza order
+            Console.WriteLine("Welcome to Papa Gino's, voted the best pizza in New England!");
+            Console.WriteLine($"Orderd a size {size} pizza with the following toppings: {string.Join(", ", toppings)}", size, toppings);
+            Console.WriteLine("Order Complete!  Pickup in 10 minutes.");
+        }
+    }
+}
---
+++ src/PizzaStore/PizzaMakers/ZPizza.cs
@@ -0,0 +1,17 @@
+﻿using PizzaDomain;
+using System;
+using System.Collections.Generic;
+
+namespace PizzaStore.PizzaMakers
+{
+    public class ZPizza : IPizzaMaker
+    {
+        public void TakeOrder(Size size, List<string> toppings)
+        {
+            // this would process some pizza order
+            Console.WriteLine("Welcome to ZPizza.  Voted worst pizza in Raleigh by Eric!");
+            Console.WriteLine($"Orderd a size {size} pizza with the following toppings: {string.Join(", ", toppings)}", size, toppings);
+            Console.WriteLine("Your order will take a long time and will cost you all the money in your wallet.");
+        }
+    }
+}
