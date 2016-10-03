f63d5d44ad52828d669b8835e72a7fb4be55e705
Justin Saraceno <justin.saraceno@gmail.com>
2016-10-03 14:56:33 -0400

    adding unit test

---
+++ src/PizzaStore.Tests/PizzaMaker/PapaGinosTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using PizzaDomain;
+using PizzaStore.PizzaMakers;
+using FluentAssertions;
+
+namespace PizzaStore.Tests.PizzaMaker
+{
+    [TestClass]
+    public class PapaGinosTests
+    {
+        private IPizzaMaker pizzaMaker;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            pizzaMaker = new PapaGinos();
+        }
+
+        [TestMethod]
+        public void TakeOrder_WithSizeAndToppings_ShouldBeSuccessful()
+        {
+            // arrange
+            var size = Size.Large;
+            var toppings = new List<string>();
+
+            // act
+            Action action = () => pizzaMaker.TakeOrder(size, toppings);
+
+            // assert
+            action.ShouldThrow<Exception>().WithMessage("Pizza needs toppings!");
+        }
+    }
+}
--- src/PizzaStore/PizzaMakers/PapaGinos.cs
+++ src/PizzaStore/PizzaMakers/PapaGinos.cs
@@ -8,6 +8,11 @@ namespace PizzaStore.PizzaMakers
     {
         public void TakeOrder(Size size, List<string> toppings)
         {
+            if(toppings.Count < 1)
+            {
+                throw new Exception("Pizza needs toppings!");
+            }
+
             // this would process some pizza order
             Console.WriteLine("Welcome to Papa Gino's, voted the best pizza in New England!");
             Console.WriteLine($"Orderd a size {size} pizza with the following toppings: {string.Join(", ", toppings)}", size, toppings);
